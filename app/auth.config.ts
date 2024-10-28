9a726b0627335fec3c38a8bbcbc89ce48dec91f2
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-10-29 02:06:34 +0300

    Update auth

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -5,20 +5,26 @@ export const authConfig = {
         signIn: "/login",
     },
     callbacks: {
-        authorized({ auth, request: { nextUrl } }) {
-          const isLoggedIn = !!auth?.user
-          const isOnDashboard = nextUrl.pathname.startsWith("/dashboard")
-          if (isOnDashboard) {
-            if (isLoggedIn) {
-              return true
-              // return Response.redirect(new URL("/dashboard", nextUrl))
-            }
-            return false // Redirect unauthenticated users to login page
-          } else if (isLoggedIn) {
-            return Response.redirect(new URL("/dashboard", nextUrl))
+      authorized({ auth, request: { nextUrl } }) {
+        const isLoggedIn = !!auth?.user
+        const isOnDashboard = nextUrl.pathname.startsWith("/dashboard")
+        if (isOnDashboard) {
+          if (isLoggedIn) {
+            return true
           }
-          return true
-        },
+          return false // Redirect unauthenticated users to login page
+        }
+        return true
       },
+      redirect({ url, baseUrl }) {
+        // If the redirect URL is set to the root page, let signOut redirect there
+        if (url === "/") {
+            return url
+        }
+    
+        // Otherwise, redirect to the dashboard by default after a successful signIn
+        return url.startsWith(`${baseUrl}/dashboard`) ? url : `${baseUrl}/dashboard`
+      },
+    },
       providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig
